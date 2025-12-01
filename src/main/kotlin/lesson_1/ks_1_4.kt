632a65eeba036955ac775bc229f3bd896a9b8b9b
Zats1954 <alex.zacepin@gmail.com>
2025-12-01 20:08:52 +0100

    KS_1_4 Gagarin's flight data

---
+++ src/main/kotlin/lesson_1/ks_1_4.kt
@@ -0,0 +1,17 @@
+package lesson_1
+
+fun main() {
+    val length: Long = 40_868_600_000L
+    val age: Byte = 27
+    val partOfDay: Float = 0.075f
+    val seconds: Short = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
+
+    println("Расстояние, которое пролетел Гагарин пока был в космосе в миллиметрах $length ")
+    println("Возраст Гагарина на момент полета (целых лет)  $age ")
+    println("Какую часть дня Гагарин провел в космосе  $partOfDay ")
+    println("Количество секунд, которое Гагарин провел в космосе   $seconds ")
+    println("Какую часть года Гагарин провел в космосе  $partOfYear ")
+    println("Апогей орбиты Гагарина в метрах  $apogee ")
+}
