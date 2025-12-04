8bc38b0f2f6e69116f6c629e462f88bd5b4b2c39
Zats1954 <alex.zacepin@gmail.com>
2025-12-04 12:24:27 +0100

    KS_1_5_inserted TIME_DIVIDER

--- src/main/kotlin/lesson_1/ks_1_5.kt
+++ src/main/kotlin/lesson_1/ks_1_5.kt
@@ -1,11 +1,12 @@
 package  lesson_1
 
 const val ALL_SECONDS: Short = 6480
+const val TIME_DIVIDER: Short = 60
 fun main() {
-    
-    val hours = (ALL_SECONDS / 3600).toShort()
-    val minutes = ((ALL_SECONDS - hours * 3600) / 60).toShort()
-    val seconds = (ALL_SECONDS - hours * 3600 - minutes * 60).toShort()
+
+    val hours = ALL_SECONDS / TIME_DIVIDER / TIME_DIVIDER
+    val minutes = (ALL_SECONDS - hours * TIME_DIVIDER * TIME_DIVIDER) / TIME_DIVIDER
+    val seconds = ALL_SECONDS - (hours * TIME_DIVIDER + minutes) * TIME_DIVIDER
 
     println( "%02d:%02d:%02d".format(hours,minutes,seconds))
 }
