3667ee17df190337a56f6b9b1ac80bb58d70d396
Zats1954 <alex.zacepin@gmail.com>
2025-12-05 17:39:30 +0100

    KS_2_3 calculate arrive time

---
+++ src/main/kotlin/lesson_2/ks_2_3.kt
@@ -0,0 +1,18 @@
+package lesson_2
+
+const val TIME_DIVIDER = 60
+const val DAY_DIVIDER = 24
+fun main() {
+    val timeDeparture  = "9:39"
+    val timeTravel = 457
+
+    val timeArrive = timeDeparture.split(":")
+        .let{it[0].toInt() * TIME_DIVIDER + it[1].toInt() + timeTravel}
+
+    var hourArrive = timeArrive / TIME_DIVIDER
+    if(hourArrive > DAY_DIVIDER) hourArrive %= DAY_DIVIDER
+
+    val minuteArrive = timeArrive % TIME_DIVIDER
+
+    println( "Час и минута прибытия поезда %02d:%02d".format(hourArrive,minuteArrive))
+}
