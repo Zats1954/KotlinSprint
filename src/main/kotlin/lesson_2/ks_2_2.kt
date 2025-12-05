1fa992197e1130cd3f041c0cf11ce4475cbd3ac6
Zats1954 <alex.zacepin@gmail.com>
2025-12-05 15:55:26 +0100

    KS_2_2 payment calculations

--- src/main/kotlin/lesson_2/ks_2_2.kt
+++ src/main/kotlin/lesson_2/ks_2_2.kt
@@ -3,8 +3,8 @@ package lesson_2
 
 
 fun main() {
-    val employees = IntArray(50){i -> 30000}
-    val trainees = IntArray(30){i -> 20000}
+    val employees = IntArray(50){30000}
+    val trainees = IntArray(30){20000}
     val personal = employees + trainees
     val sumEmployees = employees.reduce {acc, employee -> acc + employee}
     val sumPersonal = personal.reduce {acc, person -> acc + person}
@@ -12,5 +12,5 @@ fun main() {
 
     println("Расходы на выплату зарплаты постоянных сотрудников: $sumEmployees")
     println("Общие расходы по ЗП после прихода стажеров: $sumPersonal")
-    println("Средняя ЗП одного сотрудника после устройства стажеров: %.02f".format(averagePayment))
+    println("Средняя ЗП одного сотрудника после устройства стажеров: %d".format(averagePayment.toInt()))
 }
