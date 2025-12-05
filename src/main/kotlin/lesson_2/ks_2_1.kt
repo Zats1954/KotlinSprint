6be151aa3cfb1fc6320bb38c32f57eda5f9b4b20
Zats1954 <alex.zacepin@gmail.com>
2025-12-05 15:31:06 +0100

    KS_2_1_corrected pupilsMark initialization

--- src/main/kotlin/lesson_2/ks_2_1.kt
+++ src/main/kotlin/lesson_2/ks_2_1.kt
@@ -1,13 +1,10 @@
 package lesson_2
 
-import kotlin.random.Random
-
 fun main() {
-    val pupilsMark = arrayOfNulls<Int>(4)
-    for (index in pupilsMark.indices){pupilsMark[index] =  Random.nextInt(3,5) }
+    val pupilsMark =  intArrayOf(3,4,3,5)
     var sum = 0
-    for (pupilMark  in pupilsMark) { sum += pupilMark!!}
-    val avr = sum.toDouble() / pupilsMark.size
-    println("средняя оценка :" +"%.02f".format(avr))
+    for (pupilMark  in pupilsMark) { sum += pupilMark}
+    val averageMark = sum.toDouble() / pupilsMark.size
+    println("средняя оценка: %.2f".format(averageMark))
 
 }
