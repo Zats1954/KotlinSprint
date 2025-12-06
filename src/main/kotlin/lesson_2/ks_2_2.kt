558dfd2c5e19958a5d70fb875f40cf0fd1c9e0d2
Zats1954 <alex.zacepin@gmail.com>
2025-12-06 20:03:02 +0100

    KS_2_2 changed arrays to values

--- src/main/kotlin/lesson_2/ks_2_2.kt
+++ src/main/kotlin/lesson_2/ks_2_2.kt
@@ -1,16 +1,18 @@
 package lesson_2
 
-
-
 fun main() {
-    val employees = IntArray(50){30000}
-    val trainees = IntArray(30){20000}
-    val personal = employees + trainees
-    val sumEmployees = employees.reduce {acc, employee -> acc + employee}
-    val sumPersonal = personal.reduce {acc, person -> acc + person}
-    var averagePayment = sumPersonal.toDouble() / personal.size
+    val employees = 50
+    val employeeSalary = 30000
+    val trainees =  30
+    val traineeSalary =  20000
 
-    println("Расходы на выплату зарплаты постоянных сотрудников: $sumEmployees")
-    println("Общие расходы по ЗП после прихода стажеров: $sumPersonal")
+    val personal = employees + trainees
+    val sumEmployees = employees * employeeSalary
+    val sumTrainees = trainees * traineeSalary
+    val sumPersonal = sumEmployees + sumTrainees
+    val averagePayment = sumPersonal.toDouble() / personal
+
+    println("Расходы на выплату зарплаты постоянных сотрудников: %d".format(sumEmployees))
+    println("Общие расходы по ЗП после прихода стажеров: %d".format(sumPersonal))
     println("Средняя ЗП одного сотрудника после устройства стажеров: %d".format(averagePayment.toInt()))
 }
